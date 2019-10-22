b2051c73bc230c12d956a9e458d04553592ecbf3
Justin Persaud <jpersaud@expensify.com>
2019-10-22 13:11:57 -0700

    git alias and ripgrep

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,3 +21,20 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+
+# Push branch to upstream
+function push(){
+    branch=`git rev-parse --abbrev-ref HEAD`
+    if [ $branch == 'master' ]; then
+        echo "DO NOT PUSH TO MASTER"
+    else
+        git push origin $branch
+    fi
+}
+
+# Create a PR through the GH Web UI
+function pr(){
+    remote=`git remote -v | grep origin | head -1 | awk '{print $2}' | sed 's/.*:\(.*\)*/\1/' | sed 's/\.git$//'`
+    branch=`git rev-parse --abbrev-ref HEAD`
+    open "https://github.com/$remote/compare/${1:-master}...$branch?expand=1"
+}
