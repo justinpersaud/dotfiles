89f1a7933f8302937416790733fe71a467c33c2f
Justin Persaud <jpersaud@expensify.com>
2020-10-27 20:39:00 -0400

    updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,6 +21,7 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gcu='git branch --merged | egrep -v "(^\*|master)" | xargs git branch -d'
 
 # Push branch to upstream
 function push(){
